1ede8ffe51da57f3542047cb9fa255a0ace36b65
Cham <runningpaipai@gmail.com>
2024-02-16 11:29:18 +0800

    Feat: Create Franc class and test it

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -3,11 +3,12 @@ package guru.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
     @Test
-    void testMulitplication() {
+    void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(new Dollar(10), product);
@@ -16,7 +17,24 @@ public class MoneyTest {
     }
 
     @Test
-    void testEquality() {
+    void testEqualityDollar() {
         assertEquals(new Dollar(6), new Dollar(6));
+        assertNotEquals(new Dollar(9), new Dollar(6));
+    }
+
+
+    @Test
+    void testMultiplicationFranc() {
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+        product = five.times(3);
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityDollarFranc() {
+        assertEquals(new Franc(6), new Franc(6));
+        assertNotEquals(new Franc(9), new Franc(6));
     }
 }
